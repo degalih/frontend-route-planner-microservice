25368a7f81591b0190ee72ad351ff582a31ceba6
Galih Muhammad R <galihmramdan17@gmail.com>
2025-07-19 16:54:12 +0700

    feat: remove unused file

--- src/services/finances.ts
+++
@@ -1,7 +0,0 @@
-import {apiClient} from "@/services/api-client";
-
-export const financeService = {
-    async importRecord(data: any) {
-        return apiClient.post<void>('api/expenses/import', data)
-    }
-}
