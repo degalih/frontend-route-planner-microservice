d2467fd24cceed014efdd33cd759e1c2614df0eb
Galih Muhammad R <galihmramdan17@gmail.com>
2025-07-19 11:40:35 +0700

    feat: setup config for api client

---
+++ src/services/config.ts
@@ -0,0 +1,6 @@
+export const API_BASE_URL: string = process.env.API_BASE_URL ?? 'http://localhost:8000';
+
+export const DEFAULT_HEADERS = {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+}
